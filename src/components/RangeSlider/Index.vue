551579381b671122474385a3e95c5b8a4f7b92f2
Mucahit Karaman <mkaraman419@gmail.com>
2022-08-12 01:47:27 +0300

    first commit

---
+++ src/components/RangeSlider/Index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="w-full bg-blue-400">here</div>
+</template>
+<script>
+export default {
+  name: "RangeSlider",
+  props: {
+    min: {
+      default: 0,
+    },
+    max: {
+      default: 1,
+    },
+    step: {
+      default: 0.1,
+    },
+    labels: {
+      type: Array,
+      default: () => ["Min.", "Max."],
+    },
+    value: {
+      type: Array,
+      default: () => [0, 100],
+    },
+  },
+  model: {
+    prop: "value",
+  },
+};
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{vue,js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
